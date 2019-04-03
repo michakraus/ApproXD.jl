dd689236eb928138ed07380d132ad739497c1e2d
Michael Kraus <michael.kraus@ipp.mpg.de>
2019-04-03 10:44:03 +0200

    julia v1.0 fix in polyharmonic spline

--- src/polyharmonic.jl
+++ src/polyharmonic.jl
@@ -3,14 +3,14 @@
 # there was no license file.
 
 
-type PolyharmonicSpline
+mutable struct PolyharmonicSpline
   dim::Int64
   order::Int64
   coeff::Vector{Float64}
   centers::Array{Float64,2}
   error::Float64
 end
- 
+
 function polyharmonicK(r,K)
   if iseven(K)
     return r< 1 ? (r.^(K-1))*log(r.^r) : (r^K)*log(r)
@@ -18,14 +18,14 @@ function polyharmonicK(r,K)
     return r^K
   end
 end
- 
+
 function PolyharmonicSpline(K::Int64, centers::Array{Float64,2},values::Array{Float64}; s = 0.0)
   m,n = size(centers)
   m != length(values) && throw(DimensionMismatch())
- 
+
   M = zeros(m,m)
   N = zeros(m,n+1)
- 
+
   for i=1:m
     N[i,1] = 1
     N[i,2:end] = centers[i,:]
@@ -35,9 +35,9 @@ function PolyharmonicSpline(K::Int64, centers::Array{Float64,2},values::Array{Fl
   end
   M = M .+ s.*eye(m)
   L = [[M N],[N' zeros(n+1,n+1)]]
- 
+
   w = pinv(L)*[values,zeros(n+1)]
- 
+
   ivalues = zeros(m)
   for i=1:m
     tmp = 0.0
@@ -51,19 +51,19 @@ function PolyharmonicSpline(K::Int64, centers::Array{Float64,2},values::Array{Fl
     ivalues[i] = tmp
   end
   error = norm(values .- ivalues)
- 
+
   return PolyharmonicSpline(n,K,w,centers,error)
 end
- 
+
 function PolyharmonicSpline(K::Int64, centers::Vector{Float64},values::Vector{Float64};s = 0.0)
   PolyharmonicSpline(K,centers'',values,s=s)
 end
- 
+
 function interpolate(S::PolyharmonicSpline,x::Array{Float64,2})
   m,n = size(x)
- 
+
   n != S.dim && throw(DimensionMismatch("$m != $(S.dim)"))
- 
+
   interpolates = zeros(m)
   for i=1:m
     tmp = 0.0
@@ -79,15 +79,15 @@ function interpolate(S::PolyharmonicSpline,x::Array{Float64,2})
   end
   return interpolates
 end
- 
+
 function interpolate(S::PolyharmonicSpline,x::Vector{Float64})
   return interpolate(S,x'')
 end
- 
+
 function interpolate(S::PolyharmonicSpline,x::Vector{Float64},y::Vector{Float64})
   return interpolate(S,[x y])
 end
- 
+
 function interpolate(S::PolyharmonicSpline,x::Vector{Float64},y::Vector{Float64},z::Vector{Float64})
   return interpolate(S,[x y z])
 end
